aa09a86448484e006747ef02efb5991f7a3042dc
Larisa Melnichenko <erlara@ukr.net>
2023-04-05 10:14:44 +0300

    fix

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -6,7 +6,7 @@ import { deleteTask, toggleCompleted } from 'redux/operations';
 export const Task = ({ task }) => {
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteTask(task.id));
-  const handleToggle = () => dispatch(toggleCompleted(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task));
 
   return (
     <div className={css.wrapper}>
